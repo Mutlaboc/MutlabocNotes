97e2778ad4e4490b660f4fb6e7d41450e11e1b4a
Юрий <Yurik.dom@gmail.com>
2025-09-17 16:19:50 +0300

    Работа с полем даты 3 17.09.25

--- app/src/main/java/com/example/mutlabocsnotes/EditNoteScreen.kt
+++ app/src/main/java/com/example/mutlabocsnotes/EditNoteScreen.kt
@@ -1,5 +1,6 @@
 package com.example.mutlabocsnotes
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -38,6 +39,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.ui.platform.LocalContext
 import android.app.DatePickerDialog
 import java.util.Calendar
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 @OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
@@ -142,6 +145,7 @@ fun EditNoteScreen(
                 "Дела" -> {
 
                     val context = LocalContext.current
+                    val focusManager = LocalFocusManager.current
                     var todayCalendar = remember {
                         Calendar.getInstance().apply {
                             set(Calendar.HOUR_OF_DAY, 0)
@@ -193,6 +197,18 @@ fun EditNoteScreen(
                     }
                     var isRepeationg by remember { mutableStateOf(false) }
                     val dateFieldInteractionSource = remember { MutableInteractionSource() }
+                    val openDatepicker = {
+                        val selectedCalendar = Calendar.getInstance().apply {
+                            timeInMillis = selectedDeadlineMillis
+                        }
+                        datePickerDialog.updateDate(
+                            selectedCalendar.get(Calendar.YEAR),
+                            selectedCalendar.get(Calendar.MONTH),
+                            selectedCalendar.get(Calendar.DAY_OF_MONTH)
+                        )
+                        focusManager.clearFocus()
+                        datePickerDialog.show()
+                    }
                     Row(verticalAlignment = Alignment.CenterVertically) {
                         OutlinedTextField(
                             value = deadline,
@@ -207,19 +223,10 @@ fun EditNoteScreen(
                             ),
                             modifier = Modifier
                                 .weight(1f)
-                                .clickable(
-                                    interactionSource = dateFieldInteractionSource,
-                                    indication = null
-                                ) {
-                                    val selectedCalendar = Calendar.getInstance().apply {
-                                        timeInMillis = selectedDeadlineMillis
+                                .pointerInput(Unit) {
+                                    detectTapGestures {
+                                        openDatepicker
                                     }
-                                    datePickerDialog.updateDate(
-                                        selectedCalendar.get(Calendar.YEAR),
-                                        selectedCalendar.get(Calendar.MONTH),
-                                        selectedCalendar.get(Calendar.DAY_OF_MONTH)
-                                    )
-                                    datePickerDialog.show()
                                 },
                             readOnly = true,
                             interactionSource = dateFieldInteractionSource
